2fa891668dfc540b445b443fe19c3723096d3560
크앙 <cooksh2421@naver.com>
2019-02-27 15:09:06 +0900

    메인페이지 연결

--- src/main/java/com/moviecube/main/MainController.java
+++ src/main/java/com/moviecube/main/MainController.java
@@ -1,8 +1,22 @@
 package com.moviecube.main;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
+	
+	ModelAndView mav = new ModelAndView();
+	
+	@RequestMapping("/main.do")
+	public ModelAndView mainForm(HttpServletRequest request) throws Exception{		
+		
+		mav.setViewName("main");
+		
+		return mav;
+	}
 
 }
