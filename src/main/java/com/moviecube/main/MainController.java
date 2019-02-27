087e173b8791090dda71c4d02e8f418bd2b3cf4c
jungwone <prsjb77@gmail.com>
2019-02-27 15:03:32 +0900

    승현이형 이거요

--- src/main/java/com/moviecube/main/MainController.java
+++ src/main/java/com/moviecube/main/MainController.java
@@ -1,8 +1,26 @@
 package com.moviecube.main;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.moviecube.common.CommandMap;
+
+
 
 @Controller
 public class MainController {
+	@RequestMapping(value = "/main.do")
+	public ModelAndView openBoardList(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/main");
+
+		//List<Map<String, Object>> list = sampleService.selectBoardList(commandMap.getMap());
+		//mv.addObject("list", list);
+
+		return mv;
+	}
 
 }
