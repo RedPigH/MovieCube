477755456d92a0265ed1d888c2dca6a627992fca
크앙 <cooksh2421@naver.com>
2019-02-28 13:59:44 +0900

    action-servlet 변경, css/js 연결 방법 변경

--- src/main/java/com/moviecube/main/MainController.java
+++ src/main/java/com/moviecube/main/MainController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
+@EnableWebMvc
 public class MainController {
 	
 	ModelAndView mav = new ModelAndView();
