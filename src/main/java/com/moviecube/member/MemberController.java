840d0659e908225c5cffc02867aa22d2a0189872
주현 <>
2019-03-07 17:40:42 +0900

    회원가입 중복확인 빼고 완성

--- src/main/java/com/moviecube/member/MemberController.java
+++ src/main/java/com/moviecube/member/MemberController.java
@@ -11,30 +11,33 @@ import com.moviecube.common.CommandMap;
 
 @Controller
 public class MemberController {
-	/*
-	 * Logger log = Logger.getLogger(this.getClass());
-	 * 
-	 * @Resource(name="memberService") private MemberServiceImpl memberService;
-	 * 
-	 * //동의서
-	 * 
-	 * @RequestMapping(value="/openTerms.do") public ModelAndView terms(CommandMap
-	 * commandMap) throws Exception{ ModelAndView mv = new
-	 * ModelAndView("/member/terms");
-	 * 
-	 * return mv; }
-	 * 
-	 * //회원가입 폼
-	 * 
-	 * @RequestMapping(value="/member/joinForm.do") public ModelAndView
-	 * joinForm(CommandMap commandMap) throws Exception{ ModelAndView mv = new
-	 * ModelAndView("/member/joinForm");
-	 * 
-	 * return mv; }
-	 * 
-	 * @RequestMapping(value="/member/join.do") public String join(CommandMap
-	 * commandMap) throws Exception{ try {
-	 * memberService.insertMember(commandMap.getMap()); } catch(Exception e) { }
-	 * return "redirect:/main"; }
-	 */
+	
+	  Logger log = Logger.getLogger(this.getClass());
+	  
+	  @Resource(name="memberService")
+	  private MemberServiceImpl memberService;
+	  
+	  //동의서
+	  
+	  @RequestMapping(value="/openTerms.do")
+	  public ModelAndView terms(CommandMap commandMap) throws Exception{
+		  ModelAndView mv = new ModelAndView("/member/terms");
+	  
+		  return mv;
+	  }
+	  
+	  //회원가입 폼
+	  
+	  @RequestMapping(value="/member/joinForm.do")
+	  public ModelAndView joinForm(CommandMap commandMap) throws Exception{
+		  ModelAndView mv = new ModelAndView("/member/joinForm");
+	  
+		  return mv;
+	  }
+	  	
+	  @RequestMapping(value="/member/join.do") public String join(CommandMap
+	  commandMap) throws Exception{ try {
+	  memberService.insertMember(commandMap.getMap()); } catch(Exception e) { }
+	  return "redirect:/main.do"; }
+	 
 }
--- src/main/java/com/moviecube/member/MemberDAO.java
+++ src/main/java/com/moviecube/member/MemberDAO.java
@@ -4,16 +4,19 @@ import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.moviecube.dao.AbstractDAO;
 
+@Repository("memberDAO")
 public class MemberDAO extends AbstractDAO {
-	/*
-	 * protected Log log = LogFactory.getLog(MemberDAO.class);
-	 * 
-	 * @Autowired private SqlSessionTemplate sqlSession;
-	 * 
-	 * // 회원가입 public void insertMember(Map<String, Object> map) throws Exception {
-	 * insert("member.insertMember", map); }
-	 */
+	
+/*	  protected Log log = LogFactory.getLog(MemberDAO.class);*/
+	  
+	  @Autowired private SqlSessionTemplate sqlSession;
+	  
+	  // 회원가입
+	  public void insertMember(Map<String, Object> map) throws Exception {
+	  insert("member.insertMember", map);
+	  } 
 }
