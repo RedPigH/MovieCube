691d89ecf8bf20e3725e41224a6f13d835d867ff
hpig <gtv7854@naver.com>
2019-03-12 13:56:44 +0900

    로그 : 전자정부프레임워크 주석처리 좌석 Controller 작업중

--- src/main/java/com/moviecube/seat/SeatDAO.java
+++ src/main/java/com/moviecube/seat/SeatDAO.java
@@ -11,10 +11,41 @@ import com.moviecube.dao.AbstractDAO;
 
 @Repository("seatDAO")
 public class SeatDAO extends AbstractDAO {
-	
+
 	@SuppressWarnings("unchecked")
-	public List<Map<String,Object>> selectScreenSeat(Map<String,Object> map) throws Exception{
-		return (List<Map<String,Object>>) selectList("selectScreenSeat", map);
+	public List<Map<String, Object>> selectScreenSeat(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("seat.selectScreenSeat", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> ScreenSeatNum(Map<String, Object> map) throws Exception {
+		return (Map<String, Object>) selectOne("seat.ScreenSeatNum", map);
 	}
 	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectTimeSeat(Map<String, Object> map) throws Exception {
+		return (List<Map<String, Object>>) selectList("seat.selectTimeSeat", map);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<String, Object> selectSeat(Map<String, Object> map) throws Exception {
+		return (Map<String, Object>) selectOne("seat.selectSeat", map);
+	}
+
+	public void insertSeat(Map<String, Object> map) throws Exception {
+		insert("seat.insertSeat", map);
+	}
+
+	public void insertTimeSeat(Map<String, Object> map) throws Exception {
+		insert("seat.insertTimeSeat", map);
+	}
+
+	public void updateSeatStatus(Map<String, Object> map) throws Exception {
+		update("seat.updateSeatStatus", map);
+	}
+
+	public void deleteSeat(Map<String, Object> map) throws Exception {
+		delete("seat.deleteSeat", map);
+	}
+
 }
