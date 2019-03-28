d90c7567144428af0b7f123f2005bb0cb3121bf5
hpig <gtv7854@naver.com>
2019-03-28 12:40:31 +0900

    no message

---
+++ src/main/java/com/moviecube/store/StoreService.java
@@ -0,0 +1,22 @@
+package com.moviecube.store;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface StoreService {
+	
+	List<Map<String, Object>> selectStoreList(Map<String, Object> map) throws Exception;
+	
+	void insertStore(Map<String, Object> map, HttpServletRequest request) throws Exception;
+	
+	Map<String, Object> selectStoreDetail(Map<String,Object> map) throws Exception;
+	
+	public void modifyStore(Map<String, Object> map, HttpServletRequest request) throws Exception;
+	
+	public void deleteStore(Map<String, Object> map, HttpServletRequest request) throws Exception;
+	
+	List<Map<String, Object>> storeSearch0(String map) throws Exception;
+	
+}
